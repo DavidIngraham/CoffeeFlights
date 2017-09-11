3cef30fe81adf079499deea8d527ba0640e990d2
David Ingraham <davingrahamd@gmail.com>
2017-09-10 19:25:23 -0700

    Safely Ignore Stupid Planes that dont broadcast their tail number

--- CoffeeFlights/AirCraftList.py
+++ CoffeeFlights/AirCraftList.py
@@ -17,24 +17,34 @@ class AirCraftList:
 
     def update(self, new_aircraft_list_dict):
         # Add any New AirCraft
-        for aircraft_dict in new_aircraft_list_dict:
+        for new_aircraft_dict in new_aircraft_list_dict:
             new = True
-            tail_number = aircraft_dict['Reg']
-            for i in self.aircraft_object_list:
-                if i.tail_number == tail_number:
-                    new = False
-                    break
+            try:
+                tail_number = new_aircraft_dict['Reg']
+            except KeyError as e:
+                print(e)
+                new = False
+            else:
+                for i in self.aircraft_object_list:
+                    if i.tail_number == tail_number:
+                        new = False
+                        break
             if new:
-                self.aircraft_object_list.append(AirCraft(aircraft_dict))
+                self.aircraft_object_list.append(AirCraft(new_aircraft_dict))
         # Update any aircraft already in list. Iterate backwards so we can delete planes that longer tracked
         for i in reversed(range(len(self.aircraft_object_list))):
             current = False
             # Iterate through all new data to check if the Tail number is still there
             for new_aircraft in new_aircraft_list_dict:
-                if self.aircraft_object_list[i].tail_number == new_aircraft['Reg']:
-                    current = True
-                    self.aircraft_object_list[i].update(new_aircraft)
-                    break
+                try:
+                    new_aircraft_tail_number = new_aircraft['Reg']
+                except KeyError as e:
+                    print(e)
+                else:
+                    if self.aircraft_object_list[i].tail_number == new_aircraft_tail_number:
+                        current = True
+                        self.aircraft_object_list[i].update(new_aircraft)
+                        break
             if not current:
                 del(self.aircraft_object_list[i])
 
