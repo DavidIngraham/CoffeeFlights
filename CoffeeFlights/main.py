089cee7fd88dd7846fcde4f253f11392a6df6432
David Ingraham <davingrahamd@gmail.com>
2017-09-28 09:11:25 -0700

    Add Plotting Demo

--- CoffeeFlights/main.py
+++ CoffeeFlights/main.py
@@ -1,16 +1,31 @@
 import time
+import matplotlib.pyplot as plt
 from CoffeeFlights.Radar import Radar
 
 radar = Radar()
 
+bounds = (33.465673, 33.4074764, -111.848405, -112.030175)  # NSEW Bounds
 
 if __name__ == '__main__':
+
     while True:
-        radar.update_aircraft_list()
         print('Updating:', time.ctime())
+        radar.update_aircraft_list()
+        plt.ion()
 
-        for plane in radar.aircraft_list:
-            print(plane.call_sign, plane.lat, plane.lng, plane.alt_msl, plane.model)
+        for foo in range(0, 20):
+            axes = plt.gca()
+            axes.set_xlim([bounds[3], bounds[2]])
+            axes.set_ylim([bounds[1], bounds[0]])
+            radar.aircraft_list.interpolate_all()
+            for plane in radar.aircraft_list:
+                print(plane.call_sign, plane.lat, plane.lng, plane.alt_msl, plane.model)
+                plt.scatter(plane.lng, plane.lat)
+                plt.pause(0.001)
 
-        time.sleep(1)
+            plt.draw()
+            plt.pause(.2)
+            plt.clf()
+
+
 
